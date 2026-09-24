d80e07ede6021faa46fb71e5e541567c88d8bab5
Moiz <mahmed4939@hwdsb.on.ca>
2026-09-24 13:41:18 -0400

    abc

--- delme.py
+++ delme.py
@@ -1,1 +1,10 @@
-print("hello")
+class Student:
+def __init__(self, name, grade):
+self.name = name
+self.grade = grade
+def __str__(self):
+return f"{self.name} is in grade {self.grade}"
+Creating an object from the blueprint
+a = Student("Bella", 10)
+Printing the object automatically triggers the __str__ method
+print(a)
